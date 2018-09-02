1cc9a105b300ee044d6cf0768c4456eb4ded6677
vladimirvorovalnitsev <vladimirvorovalnitsev@gmail.com>
2018-09-02 13:54:14 +0300

    First commit

--- config/index.js
+++ config/index.js
@@ -35,8 +35,8 @@ function getSyslogSocketParameterTest() {
 
 function getSyslogSocketParameterProduction() {
     return {
-        address: '207.154.224.201',
-        port: '514',
+        address: 'localhost',
+        port: '1514',
         exclusive: 'false'
     }
 }
